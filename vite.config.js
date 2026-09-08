c8b65090523f014b0e2e74f2e2110b8d2e8d77f5
vasanthveeravfx-glitch <vasanthveeravfx@gmail.com>
2026-09-09 00:26:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-base: '/portfolio2/',
+
 export default defineConfig({
+base: '/portfolio2/',
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
