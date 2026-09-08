af7ff8c9d056e217182b91b7f935c9ce58bb6e89
vasanthveeravfx-glitch <vasanthveeravfx@gmail.com>
2026-09-09 00:24:39 +0530

    Set base path for Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+base: '/portfolio2/',
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
